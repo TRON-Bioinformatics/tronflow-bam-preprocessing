5d20362098b40d4675a65dcfd813917f3fd24802
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-04-07 14:29:43 +0200

    fix tests

--- tests/test_01.sh
+++ tests/test_01.sh
@@ -8,7 +8,6 @@ nextflow main.nf -profile test,conda --output $output
 test -s $output/sample1/sample1.preprocessed.bam || { echo "Missing output BAM file!"; exit 1; }
 test -s $output/sample1/sample1.preprocessed.bai || { echo "Missing output BAI file!"; exit 1; }
 test -s $output/sample1/software_versions.PREPARE_BAM.txt || { echo "Missing software versions file!"; exit 1; }
-test -s $output/sample1/software_versions.INDEX_BAM.txt || { echo "Missing software versions file!"; exit 1; }
 test -s $output/sample1/software_versions.MARK_DUPLICATES.txt || { echo "Missing software versions file!"; exit 1; }
 test -s $output/sample1/software_versions.HS_METRICS.txt || { echo "Missing software versions file!"; exit 1; }
 test -s $output/sample1/software_versions.METRICS.txt || { echo "Missing software versions file!"; exit 1; }
